7b062ba60c737307fc468da4fa9d33c9ae72d4ab
keerthi ramisetti <keerthiramisetti1234@gmail.com>
2024-12-10 16:36:58 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-export { default as Button } from './components/Button';
-export { default as Checkbox } from './components/Checkbox';
-export { default as Input } from './components/Input';
+export { default as Buttons } from './components/Buttons';
+export { default as Dropdown } from './components/Dropdown';
+export { default as InputFields } from './components/InputFields'
+export { default as RadioButton } from './components/RadioButton';
+export { default as TextArea } from './components/TextArea';
+export { default as ToggleSwitch } from './components/ToggleSwitch';
+export { default as Checkbox } from './components/Checkbox'
